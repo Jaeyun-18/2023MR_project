bfc7683672a917a010c73fe7ea0f5a2f8f0fcd42
LEE SEOHEE <seohee.lee003@gmail.com>
2023-09-25 23:04:19 +0900

    restoring main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ left_landmarks = landmark_translate(True, ["S1", "S2", "E1", "W1"])
 front_landmarks = landmark_translate(
     True, ["S1", "S2", "E1", "E2", "W1", "W2", "H1", "H2"])
 
-# left = PoseGetter(3, "left", left_landmarks)
+left = PoseGetter(3, "left", left_landmarks)
 front = PoseGetter(0, "front", front_landmarks)
 
 
 while front.is_open():
-    # left_points, left_img = left.run_cycle(True)
+    left_points, left_img = left.run_cycle(True)
     front_points, front_img = front.run_cycle(True)
 
     # marking
@@ -24,13 +24,13 @@ while front.is_open():
     H1_F = front_points[6]
     H2_F = front_points[7]
 
-    """S1_L = left_points[0]
+    S1_L = left_points[0]
     S2_L = left_points[1]
     E1_L = left_points[2]
-    W1_L = left_points[3]"""
+    W1_L = left_points[3]
 
     front.show_vid(None)
-    # cv2.imshow('front image', cv2.flip(front_img, 1))
+    cv2.imshow('front image', cv2.flip(front_img, 1))
 
     try:
         print(front_points[1])
