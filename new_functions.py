6c8beca5010b7fc33d9ce31ca2aa191fb0024a4d
kipperjjang <kipperjjang05@gmail.com>
2023-11-13 21:20:35 +0900

    new_function modified

---
+++ new_functions.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+#right motor ID(up to down) - even(0,2,4,6)
+
+#left motor ID(up to down) - odd(1,3,5,7)
+
+
+def cal_LS_0(): #leftNright
+    
+    return
+
+def cal_LS_2(): #upNdown
+    
+    return
+
+def cal_LE_4(): #upNdown
+    
+    return
+
+def cal_LE_6(): #leftNright
+    
+    return
+
+def cal_RS_1(): #leftNright
+    
+    return
+
+def cal_RS_3(): #upNdown
+    
+    return
+
+def cal_RE_5(): #upNdown
+    
+    return
+
+def cal_RE_7(): #leftNright
+    
+    return 
