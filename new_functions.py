5bca1a2da6ef066ede4fd5cd539ae613197a0232
kipperjjang <kipperjjang05@gmail.com>
2023-11-22 00:55:45 +0900

    new_function modified

--- new_functions.py
+++ new_functions.py
@@ -136,8 +136,14 @@ def cal_RE_57(left_shoulder_coor, right_hip_coor, right_shoulder_coor, right_elb
     Body_vec = right_shoulder_coor - right_hip_coor #right hip to right shoulder body vector
     Front_vec = np.cross(Body_vec, Shoulder_vec) #back to front(y-axis)
 
-    Theta_R = rad2six(np.arccos(np.dot(upper_arm_vector, lower_arm_vector)))  # upNdown angle
-
-    Phi_R = rad2six(np.arccos(np.dot(vector_z, Front_vec))) # leftNright angle
+    Shoulder_cross_vec = np.cross(Shoulder_vec,upper_arm_vector) / \
+        (np.linalg.norm(Shoulder_vec)*np.linalg.norm(upper_arm_vector))
+    Elbow_cross_vec = np.cross(lower_arm_vector, upper_arm_vector) / \
+        (np.linalg.norm(lower_arm_vector)*np.linalg.norm(upper_arm_vector))
+        
+    Phi_R = rad2six((np.arccos(np.dot(Shoulder_cross_vec, Elbow_cross_vec) / \
+        (np.linalg.norm(Shoulder_cross_vec)*np.linalg.norm(Elbow_cross_vec))))) + (np.pi/2) # leftNright angle
+    
+    return Theta_R, Phi_R
 
     return Theta_R, Phi_R
