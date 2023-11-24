daeb572a32c865dae65f65eecfd7e17825843d65
Jaeyun-18 <min070118@gmail.com>
2023-11-24 16:02:27 +0900

    aa

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import cv2
 from mp_handler import *
 import numpy as np
 from typing import Tuple
-import thread
+import threading
+import queue
 
 from stereo_camera import StereoCameraSystem
 import matplotlib.pyplot as plt
@@ -10,6 +11,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from time import time
 from graph_plot import plot_3d
 from new_functions import *
+from motor_control import *
 
 landmarks = landmark_translate(
     True, ["W1", "E1", "S1", "H1", "H2", "S2", "E2", "W2"])
@@ -24,37 +26,27 @@ TestCamSys = StereoCameraSystem("right_camera", "left_camera", "cali_imgs/right_
                                 "cali_imgs/left_imgs", "cali_imgs/sync_imgs", [7, 9])
 TestCamSys.calibrate(True, "test_mtx.npz")
 
-wcs = []
-times = []
-t0 = time.time()
-ms = []
+q = queue.Queue()
+thread_motor = threading.Thread(target=mov_motor, args=(q,))
+thread_motor.daemon = True
+thread_motor.start()
 
-print(left.is_open())
-print(right.is_open())
+if __name__ == '__main__':
+    while left.is_open() and right.is_open():
+        try:
+            left_points, left_img = left.run_cycle()
+            right_points, right_img = right.run_cycle()
+            world_coord = TestCamSys.triangulate(right_points, left_points)
 
-while left.is_open() and right.is_open():
-    try:
-        left_points, left_img = left.run_cycle()
-        right_points, right_img = right.run_cycle()
-        world_coord = TestCamSys.triangulate(right_points, left_points)
+            goal_angle = cal_angle(world_coord)
+            q.put(goal_angle)
+            print(goal_angle)
+            
+            right.show_vid(None)
+            left.show_vid(None)
 
-        goal_angle = cal_angle(world_coord)
-        print(goal_angle)
-        
-        right.show_vid(None)
-        left.show_vid(None)
+        except Exception as e:
+            print(e)
 
-    except Exception as e:
-        print(e)
-
-    if cv2.waitKey(5) == ord('q'):
-        break
-
-plot_3d(wcs, times)
-
-ms = np.array(ms)
-ax = plt.plot(ms[:, 0])
-ax = plt.plot(ms[:, 1])
-ax = plt.plot(ms[:, 2])
-ax = plt.plot(ms[:, 3])
-plt.show()
+        if cv2.waitKey(5) == ord('q'):
+            break
