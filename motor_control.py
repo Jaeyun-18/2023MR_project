a2df7e477e631ea11d0688219aa8de32f15673e6
Jaeyun-18 <min070118@gmail.com>
2023-11-24 16:31:39 +0900

    change motor_control

--- motor_control.py
+++ motor_control.py
@@ -2,6 +2,28 @@ import os
 import math
 import queue
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
+import msvcrt
+
+# Control table address for Dynamixel MX
+TORQUE_ENABLE_AX       = 24               # Control table address is different in Dynamixel model
+GOAL_POSITION_AX       = 30
+PRESENT_POSITION       = 36
+
+# Protocol version
+PROTOCOL_VERSION           = 1.0               #  See which protocol version is used in the Dynamixel
+
+# Default setting 
+
+
+BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
+DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+TORQUE_ENABLE               = 1                 # Value for enabling the torque
+TORQUE_DISABLE              = 0                 # Value for disabling the torque 
+DXL_MOVING_STATUS_THRESHOLD = 10               # Dynamixel MX moving status threshold
+
+index = 0
 
 goal_angle = [ # initial angle reset(차렷 자세)
         [0], #0
@@ -25,65 +47,30 @@ initial_angle=[ # initial angle reset(차렷 자세)
         [0]  #7
     ]
 
+def angle(value):
+    return (value+150)/300*1024
+
+def translator(angle_list,act_angle_list,DXL_ID):
+    for i in range(len(DXL_ID)):
+        act_angle_list.append([])
+        for j in angle_list[i]:
+            act_angle_list[-1].append(math.floor(angle(j)))    
+    
+    return act_angle_list
+
+def getch():
+    return msvcrt.getch().decode()
+
 def mov_motor(q):
-    if os.name == 'nt':
-        import msvcrt
-        def getch():
-            return msvcrt.getch().decode()
-    else:
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        def getch():
-            try:
-                tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(1)
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
-
-
-    # Control table address for Dynamixel MX
-    TORQUE_ENABLE_AX       = 24               # Control table address is different in Dynamixel model
-    GOAL_POSITION_AX       = 30
-    PRESENT_POSITION       = 36
-
-    # Protocol version
-    PROTOCOL_VERSION           = 1.0               #  See which protocol version is used in the Dynamixel
-
-    # Default setting 
-
-
-    BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
-    DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-                                                    # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
-
-    TORQUE_ENABLE               = 1                 # Value for enabling the torque
-    TORQUE_DISABLE              = 0                 # Value for disabling the torque 
-    DXL_MOVING_STATUS_THRESHOLD = 10               # Dynamixel MX moving status threshold
-
-
-    index = 0
-    def angle(value):
-        return (value+150)/300*1024
-
-    def translator(angle_list,act_angle_list):
-        for i in range(len(DXL_ID)):
-            act_angle_list.append([])
-            for j in angle_list[i]:
-                act_angle_list[-1].append(math.floor(angle(j)))    
-        
-        return act_angle_list
-
     DXL_ID= [0,2,4,6,1,3,5,7] # right - even, left - odd
     if(q.empty()): pass
     else: goal_angle=q.get()
 
     initial_angle_position = []
-    initial_angle_position = translator(initial_angle, initial_angle_position)
+    initial_angle_position = translator(initial_angle, initial_angle_position, DXL_ID)
 
     dxl_goal_position = []
-    dxl_goal_position = translator(goal_angle, dxl_goal_position)
+    dxl_goal_position = translator(goal_angle, dxl_goal_position, DXL_ID)
     
     # Initialize PortHandler instance
     # Set the port paths
@@ -94,26 +81,6 @@ def mov_motor(q):
     # Set the protocol version
     # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
     packetHandler = PacketHandler(PROTOCOL_VERSION)
-
-    # Open port
-    if portHandler.openPort():
-        print("Succeeded to open the port")
-    else:
-        print("Failed to open the port")
-        print("Press any key to terminate...")
-        getch()
-        quit()
-
-
-    # Set port baudrate
-    if portHandler.setBaudRate(BAUDRATE):
-        print("Succeeded to change the baudrate")
-    else:
-        print("Failed to change the baudrate")
-        print("Press any key to terminate...")
-        getch()
-        quit()
-
     # Enable Dynamixel#1 Torque
     for ID_number in DXL_ID:
         dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID_number, TORQUE_ENABLE_AX, TORQUE_ENABLE)
