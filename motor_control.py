e6a6f5422e7f444e77d0dad323411bf417fbbdd2
Jaeyun-18 <min070118@gmail.com>
2023-11-24 20:50:23 +0900

    motorcode

--- motor_control.py
+++ motor_control.py
@@ -24,28 +24,6 @@ TORQUE_DISABLE              = 0                 # Value for disabling the torque
 DXL_MOVING_STATUS_THRESHOLD = 10               # Dynamixel MX moving status threshold
 
 index = 0
-
-def angle(value):
-    return (value+150)/300*1024
-
-def translator(angle_list,act_angle_list,DXL_ID):
-    for i in range(len(DXL_ID)):
-        act_angle_list.append([])
-        for j in angle_list[i]:
-            act_angle_list[-1].append(math.floor(angle(j)))    
-    
-    return act_angle_list
-
-# Initialize PortHandler instance
-# Set the port paths
-# Get methods and members of PortHandlerLinux or PortHandlerWindows
-portHandler = PortHandler(DEVICENAME)
-
-# Initialize PacketHandler instance
-# Set the protocol version
-# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
-packetHandler = PacketHandler(PROTOCOL_VERSION)
-
 goal_angle = [ # initial angle reset(차렷 자세)
         [0], #0
         [-90], #2 # right arm 
@@ -70,32 +48,47 @@ initial_angle=[ # initial angle reset(차렷 자세)
 
 DXL_ID= [0,2,4,6,1,3,5,7] # right - even, left - odd
 
-initial_angle_position = []
-initial_angle_position = translator(initial_angle, initial_angle_position, DXL_ID)
+def angle(value):
+    return (value+150)/300*1024
 
+def translator(angle_list,act_angle_list):
+    for i in range(len(DXL_ID)):
+        act_angle_list.append([])
+        for j in angle_list[i]:
+            act_angle_list[-1].append(math.floor(angle(j)))    
+    
+    return act_angle_list
+
+# Initialize PortHandler instance
+# Set the port paths
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+if portHandler.openPort():
+    pass
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    pass
+
+initial_angle_position = []
+initial_angle_position = translator(initial_angle, initial_angle_position)
 def mov_motor(q):
-    while 1:        
+    while 1:
         if(q.empty()): pass
-        else: goal_angle=q.get()
+        else: goal_angle = q.get()
 
         dxl_goal_position = []
-        dxl_goal_position = translator(goal_angle, dxl_goal_position, DXL_ID)
-
-        for ID_number in range(4):
-            if(dxl_goal_position[ID_number][0]>1023):
-                dxl_goal_position[ID_number][0]=1023
-            elif(dxl_goal_position[ID_number][0]<0):
-                dxl_goal_position[ID_number][0]=0
-
+        dxl_goal_position = translator(goal_angle, dxl_goal_position)
         while 1:
             print(goal_angle)
             # Write Dynamixel#1 goal position
             for ID_number in DXL_ID:
                 dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID_number, GOAL_POSITION_AX, dxl_goal_position[DXL_ID.index(ID_number)][index])
-                if dxl_comm_result != COMM_SUCCESS:
-                    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-                elif dxl_error != 0: 
-                    print("%s" % packetHandler.getRxPacketError(dxl_error))
 
             memo=[500 for i in range(len(DXL_ID))]
             memo[DXL_ID.index(6)]=2048
@@ -109,16 +102,6 @@ def mov_motor(q):
                         dxl_present_position=memo[DXL_ID.index(ID_number)]
                     else:
                         memo[DXL_ID.index(ID_number)]=dxl_present_position
-                    
-                    if dxl_comm_result != COMM_SUCCESS:
-                        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-                    elif dxl_error != 0:
-                        print("%s" % packetHandler.getRxPacketError(dxl_error))
-                    
-                    
-                    print("[ID:%03d] GoalPos:%.02f  PresPos:%.02f" % (ID_number, (dxl_goal_position[DXL_ID.index(ID_number)][index]), dxl_present_position),end='|  ')
-                    print("torque: %d" % (dxl_torque_enable,))
-                print(' ')
                 result=1
 
                 for ID_number in DXL_ID: 
@@ -132,14 +115,6 @@ def mov_motor(q):
                 break
             else:
                 index +=1
-
-    # Disable Dynamixel#1 Torque
-    for ID_number in DXL_ID:
-        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID_number, TORQUE_ENABLE_AX, TORQUE_DISABLE)
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % packetHandler.getRxPacketError(dxl_error))
 
     # Close port
     portHandler.closePort()
