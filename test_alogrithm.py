b7118b3bd6e2a428f083e3c0fb6ff177b2cd0446
kipperjjang <kipperjjang05@gmail.com>
2023-11-24 19:15:25 +0900

    test_alg modified

--- test_alogrithm.py
+++ test_alogrithm.py
@@ -94,7 +94,7 @@ while left.is_open() and right.is_open():
         # left.show_vid({"S1": angle0, "E1": angle1})
         world_coord = TestCamSys.triangulate(right_points, left_points)
         goal_angle = cal_angle(world_coord)
-        print(goal_angle[2], goal_angle[3], goal_angle[6], goal_angle[7])
+        print(goal_angle)
         
         # motor_control(goal_angle)
         
